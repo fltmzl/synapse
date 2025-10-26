df708af55c6b49a0bbd976fd2d20a58ef36b393c
fltmzl <fltmzl2810@gmail.com>
2025-10-26 15:16:58 +0700

    fix: article link in card

--- src/app/(main)/(landing-page)/components/card/news-card.tsx
+++ src/app/(main)/(landing-page)/components/card/news-card.tsx
@@ -4,6 +4,7 @@ import { H1 } from "@/components/typography/h1";
 import { H2 } from "@/components/typography/h2";
 import { NewsItem } from "@/types/news.type";
 import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
   category: string;
@@ -14,28 +15,32 @@ type Props = {
 
 export default function NewsCard({ category, date, title, image }: Props) {
   return (
-    <div className="flex flex-col gap-5">
-      <div className="relative w-full h-[260px] overflow-hidden rounded-md">
-        <Image
-          src={image}
-          alt={title}
-          fill
-          priority
-          className="object-cover "
-        />
-      </div>
-      <div className="flex flex-col gap-2">
-        <div className="flex items-center">
-          <span className="text-base tracking-[-0.01em] leading-[150%] text-muted-foreground font-medium">
-            {category}
-          </span>
-          <span className="mx-3 w-px h-[14px] bg-border" />
-          <span className=" text-base tracking-[-0.01em] leading-[150%] text-muted-foreground font-medium">
-            {date}
-          </span>
+    <article>
+      <Link href="#">
+        <div className="flex flex-col gap-5">
+          <div className="relative w-full h-[260px] overflow-hidden rounded-md">
+            <Image
+              src={image}
+              alt={title}
+              fill
+              priority
+              className="object-cover "
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="flex items-center">
+              <span className="text-base tracking-[-0.01em] leading-[150%] text-muted-foreground font-medium">
+                {category}
+              </span>
+              <span className="mx-3 w-px h-[14px] bg-border" />
+              <span className=" text-base tracking-[-0.01em] leading-[150%] text-muted-foreground font-medium">
+                {date}
+              </span>
+            </div>
+            <H1>{title}</H1>
+          </div>
         </div>
-        <H1>{title}</H1>
-      </div>
-    </div>
+      </Link>
+    </article>
   );
 }
