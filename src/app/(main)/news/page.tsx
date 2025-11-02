81d65560d6d3d1bf0d649bbafa7cc1ca0e8ee651
Abdul Rizki <abdulrizki2002@gmail.com>
2025-11-03 00:12:29 +0700

    fix: build error with suspense boundary

--- src/app/(main)/news/page.tsx
+++ src/app/(main)/news/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
+import { Suspense } from "react";
 import AllNews from "./sections/all-news";
 import LegalNews from "./sections/legal-news";
 import SocialNetworkNews from "./sections/social-network";
 export default function NewsPage() {
   return (
-    <section className="min-h-screen p-4 flex flex-col gap-8 lg:gap-4">
+    <Suspense fallback={null}>
+      <section className="min-h-screen p-4 flex flex-col gap-8 lg:gap-4">
         <AllNews />
         <SocialNetworkNews />
         <LegalNews />
-    </section>
+      </section>
+    </Suspense>
   );
 }
