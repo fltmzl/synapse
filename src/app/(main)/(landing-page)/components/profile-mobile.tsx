79fb5181dc4217654614280c8794866af362f1c9
Abdul Rizki <abdulrizki2002@gmail.com>
2026-02-03 15:45:19 +0700

    fix: auth splash screen

--- src/app/(main)/(landing-page)/components/profile-mobile.tsx
+++ src/app/(main)/(landing-page)/components/profile-mobile.tsx
@@ -1,6 +1,7 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import useModal from "@/hooks/use-modal";
 import ProfileDialog from "./dialog/profile-dialog";
+import { Dialog } from "@radix-ui/react-dialog";
 
 type Props = {
   image: string;
@@ -11,7 +12,7 @@ export default function ProfileMobile({ image, username }: Props) {
   const { isOpen, openModal, setIsOpen } = useModal(false);
 
   return (
-    <>
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <button onClick={openModal}>
         <Avatar className="w-12 h-12">
           <AvatarImage src={image} alt={username} />
@@ -20,6 +21,6 @@ export default function ProfileMobile({ image, username }: Props) {
       </button>
 
       <ProfileDialog />
-    </>
+    </Dialog>
   );
 }
